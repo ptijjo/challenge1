82e25463d50b2ee3c87d28be3db0523008bb4e11
Joseph Bangoura <ptijjo@hotmail.com>
2024-12-07 11:09:19 +0100

    first commit

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer className='border flex w-full items-center justify-center bg-green-700'>
+            footer
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ app/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='border flex w-full items-center justify-center bg-blue-600'>
+
+            header
+        </header>
+    )
+}
+
+export default Header;
